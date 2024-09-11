94c3c538d117993434f99e154dbb54378d30aad1
Dana Litvak <dlitvak@kse.org.ua>
2024-09-11 20:33:32 +0300

    constructor of airplane class
    airplane class created, which works with helping classes row and seat
    seat -> row -> airplane

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+using namespace std;
+
+class Seat {
+    char place;
+    bool isBooked;
+    int price;
+
+public:
+    Seat(const char a, bool b, const int c)
+        : place(a),
+          isBooked(b),
+          price(c) {
+    }
+};
+
+class Row {
+    int number;
+    vector<Seat> seats;
+
+public:
+    Row(const int a, const vector<Seat> &b)
+        : number(a),
+          seats(b) {
+    }
+};
+
+class Airplane {
+    string flightNumber;
+    string destination;
+    vector<Row> rows;
+
+public:
+    Airplane(string a, string b, const vector<Row> &c)
+        : flightNumber(move(a)),
+          destination(move(b)),
+          rows(c) {
+    }
+};
+
+class Ticket {
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
